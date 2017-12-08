2cdb161ed4d517a17f63d00184dacb6f9e964054
Mario Uher <uher.mario@gmail.com>
2017-12-08 19:23:56 +0100

    Init repo

---
+++ lib/url_parser.ex
@@ -0,0 +1,18 @@
+defmodule UrlParser do
+  @moduledoc """
+  Documentation for UrlParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UrlParser.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/url_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule UrlParserTest do
+  use ExUnit.Case
+  doctest UrlParser
+
+  test "greets the world" do
+    assert UrlParser.hello() == :world
+  end
+end
