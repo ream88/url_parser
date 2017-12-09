afdf62751b1e4776bc5f0c2342027b532c157a54
Mario Uher <uher.mario@gmail.com>
2017-12-09 23:29:35 +0100

    Implement URLParser

--- lib/url_parser.ex
+++ lib/url_parser.ex
@@ -1,18 +1,6 @@
 defmodule UrlParser do
-  @moduledoc """
-  Documentation for UrlParser.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> UrlParser.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  def parse(url) when is_binary(url), do: url |> URI.parse() |> parse
+  def parse(%URI{scheme: nil} = url), do: parse("http://#{url}")
+  def parse(%URI{path: nil} = url), do: parse("#{url}/")
+  def parse(url), do: url
 end
--- test/url_parser_test.exs
+++ test/url_parser_test.exs
@@ -1,8 +1,21 @@
 defmodule UrlParserTest do
   use ExUnit.Case
-  doctest UrlParser
 
-  test "greets the world" do
-    assert UrlParser.hello() == :world
+  @result %{scheme: "http", host: "www.google.com", path: "/"}
+
+  test "it parses a full url" do
+    @result = UrlParser.parse("http://www.google.com/")
+  end
+
+  test "it parses a url without a path" do
+    @result = UrlParser.parse("http://www.google.com")
+  end
+
+  test "it parses a url without a scheme" do
+    @result = UrlParser.parse("www.google.com")
+  end
+
+  test "it parses a url without a scheme, domain and path" do
+    @result = UrlParser.parse("www.google.com")
   end
 end
