3a4b9633594ef206e31e842d45a9ae73020945e3
Mario Uher <uher.mario@gmail.com>
2017-12-10 11:19:31 +0100

    Improve readability

--- lib/url_parser.ex
+++ lib/url_parser.ex
@@ -1,6 +1,6 @@
 defmodule UrlParser do
-  def parse(url) when is_binary(url), do: url |> URI.parse() |> parse
-  def parse(%URI{scheme: nil} = url), do: parse("http://#{url}")
-  def parse(%URI{path: nil} = url), do: parse("#{url}/")
+  def parse(url) when is_binary(url), do: url |> URI.parse() |> parse()
+  def parse(%URI{scheme: nil} = url), do: "http://#{url}" |> parse()
+  def parse(%URI{path: nil} = url), do: "#{url}/" |> parse()
   def parse(url), do: url
 end
